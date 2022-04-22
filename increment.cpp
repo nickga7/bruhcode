69c2087334317d8bdf765b47e25867cbbb685324
nickga7 <104134267+nickga7@users.noreply.github.com>
2022-04-22 06:17:55 +0530

    Create increment.cpp

---
+++ increment.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+    void incrementArray(int[]);
+    void main()
+    {
+ 
+        int i;
+        int array[4] = {10, 20, 30, 40};
+        incrementArray(array);
+        for (i = 0; i < 4; i++)
+           printf("%d\t", array[i]);   
+ 
+    }
+ 
+    void incrementArray(int arr[])
+    {
+ 
+        int i;
+        for (i = 0; i < 4; i++)
+            arr[i]++; 
+ 
+    }
